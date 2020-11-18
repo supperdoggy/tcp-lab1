1c2ded0f1bca0105de84ebd35b15b4d29af815f0
m.marchyshak <m.marchyshak@pin-up.team>
2020-11-18 19:29:57 +0200

    start making server

--- src/server/server.go
+++ src/server/server.go
@@ -1,1 +1,25 @@
-package server
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type obj map[string]interface{}
+type arr []interface{}
+func main(){
+	fmt.Println("opening aneks database")
+	content, err := ioutil.ReadFile("/home/user/go/src/projects/tcp/tcp-lab1/src/files/500.json")
+	if err != nil{
+		panic(err.Error())
+	}
+	aneksMap := obj{}
+	err = json.Unmarshal(content, &aneksMap)
+	if err != nil{
+		panic(err.Error())
+	}
+	var aneksArr arr = aneksMap["anek"].([]interface{})
+	fmt.Println(aneksArr[0].(map[string]interface{})["text"])
+
+}
