f63f7eaac00284346e3b50581761e9c49f34051e
supperdoggy <supperspggy229@gmail.com>
2020-11-18 23:31:32 +0200

    finished

--- src/client/main.go
+++ src/client/main.go
@@ -1,80 +1,23 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"strconv"
+	"net"
 )
 
-type obj map[string]interface{}
+func main() {
 
-var(
-	path = "/home/user/go/src/projects/tcp/tcp-lab1/src/files"
-)
+	// Подключаемся к сокету
+	conn, _ := net.Dial("tcp", "127.0.0.1:8181")
 
-func decodeIBM866(m obj, data []byte) []string{
-	result := []string{}
-	i := 1
-	for _, v := range data{
-		fmt.Println(len(data)-i)
-		if d, ok := m[formatTo16Bit(v)];ok{
-			result = append(result, d.(string))
-		}
-		i++
-	}
-	return result
-}
-
-func sliceStringIntoString(s []string) string{
-	result := ""
-	i:=1
-	for _, v := range s{
-		fmt.Println(len(s)-i)
-		result += v
-		i++
-	}
-	return result
-}
-
-func formatTo16Bit(b byte) string{
-	return strconv.FormatInt(int64(b), 16)
-}
-
-func formatToInt16(b byte) string{
-	return strconv.FormatInt(int64(b), 16)
-}
-func main(){
-	content, err := ioutil.ReadFile(path+"/parsedData.json")
-	if err != nil{
+	text := "get joke\n"
+	// Отправляем в socket
+	if _, err := fmt.Fprintf(conn, text); err != nil {
 		panic(err.Error())
 	}
-	decodeMap := obj{}
-	if err := json.Unmarshal(content, &decodeMap);err!=nil{
-		panic(err.Error())
-	}
-	var decoded []string
-	for i:=1;i!=9;i++{
-		fmt.Println("Making", i, "file...")
-		content, err = ioutil.ReadFile(path+"/"+strconv.Itoa(i)+".txt")
-		if err != nil{
-			panic(err.Error())
-		}
-		decoded = append(decoded, decodeIBM866(decodeMap, content)...)
-	}
-	fmt.Println(len(decoded))
-	anecdotesString := sliceStringIntoString(decoded)
-	fmt.Println("Opening file...")
-	f, err := os.OpenFile("/home/user/go/src/projects/tcp/tcp-lab1/src/files/decoded.txt", os.O_WRONLY, 0755)
-	if err != nil{
-		panic(err.Error())
-	}
-	defer f.Close()
-	fmt.Println("Writing to file....")
-	if _, err = f.Write([]byte(anecdotesString));err!=nil{
-		panic(err.Error)
-	}
-	fmt.Println("Done!")
-
+	// Прослушиваем ответ
+	message, _ := bufio.NewReader(conn).ReadString('\n')
+	fmt.Print("Message from server: " + message)
+	conn.Close()
 }
--- src/server/server.go
+++ src/server/server.go
@@ -1,25 +1,73 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"net"
+	"time"
 )
 
+// for shortcuts
 type obj map[string]interface{}
 type arr []interface{}
-func main(){
-	fmt.Println("opening aneks database")
-	content, err := ioutil.ReadFile("/home/user/go/src/projects/tcp/tcp-lab1/src/files/500.json")
-	if err != nil{
-		panic(err.Error())
-	}
-	aneksMap := obj{}
-	err = json.Unmarshal(content, &aneksMap)
-	if err != nil{
-		panic(err.Error())
-	}
-	var aneksArr arr = aneksMap["anek"].([]interface{})
-	fmt.Println(aneksArr[0].(map[string]interface{})["text"])
 
+var (
+	jokes = getJokes("../files/500.json")
+)
+
+func sendAnswer(answer string, conn net.Conn) error{
+	_, err := conn.Write([]byte(answer + "\n"))
+	if err != nil{
+		return err
+	}
+	return nil
+}
+
+// returns random joke
+func pickRandomJoke() string{
+	rand.Seed(time.Now().UnixNano())
+	return jokes["anek"].([]interface{})[rand.Intn(len(jokes["anek"].([]interface{})))].(map[string]interface{})["text"].(string)
+}
+
+func getJokes(path string) obj{
+	content, err := ioutil.ReadFile(path)
+	if err != nil{
+		fmt.Println("cant read file with jokes")
+		panic(err.Error())
+	}
+	var jokes obj
+	err = json.Unmarshal(content, &jokes)
+	if err != nil{
+		fmt.Println("cant unmarshal json")
+		panic(err.Error())
+	}
+	return jokes
+}
+
+func main() {
+	fmt.Println("Launching server...")
+	ln, _ := net.Listen("tcp", ":8181")
+	defer ln.Close()
+	conn, _ := ln.Accept()
+	// listening
+	for {
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message Received:", string(message))
+		if string(message) == "get joke\n"{
+			answer := pickRandomJoke()
+
+			if err := sendAnswer(answer, conn);err!=nil{
+				fmt.Println(err.Error())
+			}
+		}else{
+			answer := "error"
+			if err := sendAnswer(answer, conn);err!=nil{
+				fmt.Println(err.Error())
+			}
+		}
+
+	}
 }
