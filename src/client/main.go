021d5720c0e4c258082dda60183cbf7c943bc85a
m.marchyshak <m.marchyshak@pin-up.team>
2020-11-19 14:49:40 +0200

    update

--- src/client/main.go
+++ src/client/main.go
@@ -4,20 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"time"
 )
 
 func main() {
 
 	// Подключаемся к сокету
 	conn, _ := net.Dial("tcp", "127.0.0.1:8181")
-
-	text := "get joke\n"
-	// Отправляем в socket
-	if _, err := fmt.Fprintf(conn, text); err != nil {
-		panic(err.Error())
+	for{
+		text := "get joke\n"
+		// Отправляем в socket
+		if _, err := fmt.Fprintf(conn, text); err != nil {
+			panic(err.Error())
+		}
+		// Прослушиваем ответ
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message from server: " + message)
+		// sends request once per minute
+		time.Sleep(1*time.Minute)
 	}
-	// Прослушиваем ответ
-	message, _ := bufio.NewReader(conn).ReadString('\n')
-	fmt.Print("Message from server: " + message)
 	conn.Close()
 }
